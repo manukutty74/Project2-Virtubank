08132986a28e6ed436d4c41c61144f322063d3e0
Manoj Kutty <manu_kutty@yahoo.com>
2018-07-10 07:32:11 -0400

    webhook id

--- controllers/bank_controller.js
+++ controllers/bank_controller.js
@@ -4,10 +4,10 @@ var db = require("../models");
 var nodemailer = require('nodemailer');
 
 
-router.use(function(req,res){  //express catch middleware if page doesn't exist
-	res.status(404);  //respond with status code
-	res.render('404'); //respond with 404 page
-});
+// router.use(function(req,res){  //express catch middleware if page doesn't exist
+// 	res.status(404);  //respond with status code
+// 	res.render('404'); //respond with 404 page
+// });
 
 //Create all our routes and set up logic within those routes where required.
 router.get("/login", function (req, res) {
@@ -16,7 +16,7 @@ router.get("/login", function (req, res) {
   res.sendFile(process.cwd() + "/public/assets/html/login.html");
 });
 
-router.get("/home", function (req, res) {
+router.get("/home", function (req, res) { 
 
   //res.send("Hello Word");
   res.sendFile(process.cwd() + "/public/assets/html/home.html");
